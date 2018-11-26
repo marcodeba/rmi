223c4eac18209856b8c000b47dd22391b6a9acbe
panning255 <panning255@jk.cn>
2018-11-26 21:51:41 +0800

    change port

--- src/main/java/rmi/Client.java
+++ src/main/java/rmi/Client.java
@@ -5,7 +5,7 @@ import java.rmi.Naming;
 public class Client {
 
     public static void main(String[] args) {
-        String url = "rmi://localhost:8888/";
+        String url = "rmi://localhost:1099/";
         try {
             // 在RMI服务注册表中查找名称为service02的对象，并调用其上的方法
             IService service02 = (IService) Naming.lookup(url + "service02");
--- src/main/java/rmi/Server.java
+++ src/main/java/rmi/Server.java
@@ -12,11 +12,11 @@ public class Server {
             // 本地主机上的远程对象注册表Registry的实例，并指定端口为8888，
             // 这一步必不可少（Java默认端口是1099），必不可缺的一步，缺少注册表创建，
             // 则无法绑定对象到远程注册表上
-            LocateRegistry.createRegistry(8888);
+            LocateRegistry.createRegistry(1099);
 
             // 把远程对象注册到RMI注册服务器上，并命名为service02
             //绑定的URL标准格式为：rmi://host:port/name(其中协议名可以省略，下面两种写法都是正确的）
-            Naming.bind("rmi://localhost:8888/service02", service02);
+            Naming.bind("rmi://localhost:1099/service02", service02);
         } catch (Exception e) {
             e.printStackTrace();
         }
