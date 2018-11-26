cc17aa9ae56f37a3bc683f8896d79c1b862f73fa
panning255 <panning255@jk.cn>
2018-11-26 18:15:39 +0800

    Initial commit

---
+++ src/main/java/rmi/ServiceImpl.java
@@ -0,0 +1,18 @@
+package rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+// UnicastRemoteObject用于导出的远程对象和获得与该远程对象通信的存根。
+public class ServiceImpl extends UnicastRemoteObject implements IService {
+
+    private String name;
+
+    public ServiceImpl(String name) throws RemoteException {
+        this.name = name;
+    }
+    
+    public String doService(String content) throws RemoteException {
+        return "server >> " + content;
+    }
+}
