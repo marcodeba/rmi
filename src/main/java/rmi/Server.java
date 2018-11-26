ddb9710e19e36576b80c7480bd03ee7816fc202c
panning255 <panning255@jk.cn>
2018-11-26 18:17:43 +0800

    add comments

--- src/main/java/rmi/Server.java
+++ src/main/java/rmi/Server.java
@@ -9,7 +9,9 @@ public class Server {
             // 实例化实现了IService接口的远程服务ServiceImpl对象
             IService service02 = new ServiceImpl("service02");
 
-            // 本地主机上的远程对象注册表Registry的实例，并指定端口为8888，这一步必不可少（Java默认端口是1099），必不可缺的一步，缺少注册表创建，则无法绑定对象到远程注册表上
+            // 本地主机上的远程对象注册表Registry的实例，并指定端口为8888，
+            // 这一步必不可少（Java默认端口是1099），必不可缺的一步，缺少注册表创建，
+            // 则无法绑定对象到远程注册表上
             LocateRegistry.createRegistry(8888);
 
             // 把远程对象注册到RMI注册服务器上，并命名为service02
@@ -18,7 +20,6 @@ public class Server {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         System.out.println("服务器向命名表注册了1个远程服务对象！");
     }
 }
