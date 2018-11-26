f891078a353d4ee4bf598d90906b41f83e6cb6e7
panning255 <panning255@jk.cn>
2018-11-26 22:38:49 +0800

    add comments

--- src/main/java/rmi/Client.java
+++ src/main/java/rmi/Client.java
@@ -8,6 +8,7 @@ public class Client {
         String url = "rmi://localhost:1099/";
         try {
             // 在RMI服务注册表中查找名称为service02的对象，并调用其上的方法
+            // 返回的是stub，是ServiceImpl的代理对象!!!!!!!!!
             IService service02 = (IService) Naming.lookup(url + "service02");
 
             Class stubClass = service02.getClass();
