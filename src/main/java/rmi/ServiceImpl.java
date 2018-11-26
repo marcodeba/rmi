8b31d42bd51dd2cf9f7b27827d0fab0d510a2338
panning255 <panning255@jk.cn>
2018-11-26 18:33:31 +0800

    add comments

--- src/main/java/rmi/ServiceImpl.java
+++ src/main/java/rmi/ServiceImpl.java
@@ -3,7 +3,13 @@ package rmi;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-// UnicastRemoteObject用于导出的远程对象和获得与该远程对象通信的存根。
+/**
+ * 远程对象必须实现 UnicastRemoteObject，这样才能保证客户端访问获得远程对象时，
+ * 该远程对象会把自身的一个拷贝以 Socket 形式传输给客户端，客户端获得的拷贝称为 stub，
+ * 而服务器端本身已经存在的远程对象成为 skeleton，此时客户端的 stub 是客户端的一个代理，
+ * 用于与服务器端进行通信，而 skeleton 是服务端的一个代理，
+ * 用于接收客户端的请求之后调用远程方法来响应客户端的请求
+ */
 public class ServiceImpl extends UnicastRemoteObject implements IService {
 
     private String name;
@@ -11,7 +17,7 @@ public class ServiceImpl extends UnicastRemoteObject implements IService {
     public ServiceImpl(String name) throws RemoteException {
         this.name = name;
     }
-    
+
     public String doService(String content) throws RemoteException {
         return "server >> " + content;
     }
